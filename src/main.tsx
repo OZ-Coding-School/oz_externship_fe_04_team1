a42095d868115e2805607dddd7f315d73f35137a
cks-wls <slo0521@naver.com>
2025-11-21 17:27:25 +0900

    build : msw 적용

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,27 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+// MSW 워커를 활성화하는 함수(개발환경에서만 실행)
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser')
+
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+})
