f6361e3b165c5d2a006524e8160d806977ac7e47
hwank1 <kijh4383@gmail.com>
2025-11-26 03:07:15 +0900

    Feat: 공통 컴포넌트 404 notFound 페이지 구현 #6

---
+++ src/components/common/notFound/noData.tsx
@@ -0,0 +1,51 @@
+import Data from '@/assets/data.svg'
+import Plus from '@/assets/plus.svg'
+import Button from '../Button'
+
+const NoDataPage = () => {
+  return (
+    <div className="flex h-[557px] w-[904px] flex-col px-[24px] pt-[24px] pb-[25px]">
+      <div className="h-[68px] w-[854px]">
+        <p className="mb-[10px] text-[18px] font-bold">데이터 없음</p>
+        <p className="text-[14px] text-gray-500">
+          아직 데이터가 없을 때 표시하는 상태
+        </p>
+      </div>
+
+      <div className="h-[438px] w-[854px]">
+        <div className="h-[32px] w-[854px]">
+          <h4 className="text-[14px]">미리보기</h4>
+        </div>
+        {/* 내용 박스 */}
+        <div className="h-[382px] w-[854px] justify-center rounded-[16px] border border-gray-200 bg-gray-50 p-[25px] text-center">
+          <div className="flex h-full flex-col items-center justify-center text-center">
+            <div className="mb-[24px]">
+              <div className="bg-primary-50 flex h-[80px] w-[80px] items-center justify-center rounded-full">
+                <img
+                  src={Data}
+                  alt="icon"
+                  className="flex h-[32px] w-[32px] items-center justify-center"
+                />
+              </div>
+            </div>
+
+            <p className="mb-[8px] text-[20px] font-bold text-gray-700">
+              아직 데이터가 없습니다
+            </p>
+            <p className="mb-[24px] text-[16px] text-gray-700">
+              첫 항목을 추가해 보세요
+            </p>
+            <div>
+              <Button size="lg" className="w-37">
+                <img src={Plus} alt="icon" />
+                새로 만들기
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NoDataPage
