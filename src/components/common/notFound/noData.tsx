e1bdd1c83fd67aa348f84ec8b778d961d43ea235
hwank1 <kijh4383@gmail.com>
2025-11-26 14:58:32 +0900

    feat: 공통 컴포넌트 로딩 ui 구현 #15

--- src/components/common/notFound/noData.tsx
+++ src/components/common/notFound/noData.tsx
@@ -4,39 +4,29 @@ import Button from '@/components/common/Button'
 
 const NoData = () => {
   return (
-    <div className="flex h-[557px] w-[904px] flex-col px-[24px] pt-[24px] pb-[25px]">
-      <div className="h-[68px] w-[854px]">
-        <p className="mb-[10px] text-[18px] font-bold">데이터 없음</p>
-        <p className="text-[14px] text-gray-500">
-          아직 데이터가 없을 때 표시하는 상태
+    <div className="h-[382px] w-[854px] justify-center rounded-[16px] border border-gray-200 bg-gray-50 p-[25px] text-center">
+      <div className="flex h-full flex-col items-center justify-center text-center">
+        <div className="mb-[24px]">
+          <div className="bg-primary-50 flex h-[80px] w-[80px] items-center justify-center rounded-full">
+            <img
+              src={Data}
+              alt="icon"
+              className="flex h-[32px] w-[32px] items-center justify-center"
+            />
+          </div>
+        </div>
+
+        <p className="mb-[8px] text-[20px] font-bold text-gray-700">
+          아직 데이터가 없습니다
         </p>
-      </div>
-
-      {/* 내용 박스 */}
-      <div className="h-[382px] w-[854px] justify-center rounded-[16px] border border-gray-200 bg-gray-50 p-[25px] text-center">
-        <div className="flex h-full flex-col items-center justify-center text-center">
-          <div className="mb-[24px]">
-            <div className="bg-primary-50 flex h-[80px] w-[80px] items-center justify-center rounded-full">
-              <img
-                src={Data}
-                alt="icon"
-                className="flex h-[32px] w-[32px] items-center justify-center"
-              />
-            </div>
-          </div>
-
-          <p className="mb-[8px] text-[20px] font-bold text-gray-700">
-            아직 데이터가 없습니다
-          </p>
-          <p className="mb-[24px] text-[16px] text-gray-700">
-            첫 항목을 추가해 보세요
-          </p>
-          <div>
-            <Button size="lg" className="w-37">
-              <img src={Plus} alt="icon" />
-              새로 만들기
-            </Button>
-          </div>
+        <p className="mb-[24px] text-[16px] text-gray-700">
+          첫 항목을 추가해 보세요
+        </p>
+        <div>
+          <Button size="lg" className="w-37">
+            <img src={Plus} alt="icon" />
+            새로 만들기
+          </Button>
         </div>
       </div>
     </div>
