8bb2cfea9f66872ca9301c49a78c95b9cb0862b3
cks-wls <slo0521@naver.com>
2025-11-26 14:16:48 +0900

    feat : api 파일 추가

--- src/mocks/handlers/user/index.ts
+++ src/mocks/handlers/user/index.ts
@@ -2,7 +2,7 @@ import { API_PATHS } from '@/constant/api'
 import { http, HttpResponse } from 'msw'
 import { userInformation } from './mockData'
 
-export const questionListHandler = [
+export const userInformationHandler = [
   http.get(API_PATHS.USER.GET, () => {
     return HttpResponse.json(userInformation)
   }),
