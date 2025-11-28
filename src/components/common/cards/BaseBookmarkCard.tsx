dafd0dd33e5bfc68cc636b65e3991a0962e99370
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-28 11:01:54 +0900

    style: 북마크 레이아웃 컴포넌트에서 높이 증가 이슈를 h-fit으로 해결

--- src/components/common/cards/BaseBookmarkCard.tsx
+++ src/components/common/cards/BaseBookmarkCard.tsx
@@ -12,7 +12,7 @@ function BaseBookmarkCard({
   children,
 }: BaseBookmarkCardProps) {
   return (
-    <div className="flex w-[318px] gap-3 rounded-xl border border-gray-200 p-[13px] md:w-[838px] md:min-w-[838px] md:max-w-[838px] md:items-center md:gap-6 md:p-[25px]">
+    <div className="flex h-fit w-[318px] gap-3 rounded-xl border border-gray-200 p-[13px] md:w-[838px] md:max-w-[838px] md:min-w-[838px] md:items-center md:gap-6 md:p-[25px]">
       <img
         className="h-[48px] w-[64px] rounded-lg object-cover md:h-[94px] md:w-[160px]"
         src={thumbnail_img_url}
