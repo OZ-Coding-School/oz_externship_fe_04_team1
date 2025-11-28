09a665b89ff70620e534c5cdd5e07412e7bf1c25
cks-wls <slo0521@naver.com>
2025-11-28 11:48:13 +0900

    feat : 항목 active 되었을때 추가(#29)

--- src/components/common/sideNavigation/MobileSideNavigation.tsx
+++ src/components/common/sideNavigation/MobileSideNavigation.tsx
@@ -1,42 +1,139 @@
 import profileIcon from '@/assets/icons/modalProfileIcon.svg'
+import activeProfileIcon from '@/assets/icons/mobileActivemodalProfileIcon.svg'
 import bookMarkIcon from '@/assets/icons/bookMark.svg'
+import activeBookMarkIcon from '@/assets/icons/mobileActiveBookMark.svg'
 import applyIcon from '@/assets/icons/applyIcon.svg'
+import activeApplyIcon from '@/assets/icons/mobileActiveApplyIcon.svg'
 import completeStudyIcon from '@/assets/icons/completeStudyIcon.svg'
-
+import activeCompleteStudyIcon from '@/assets/icons/mobileActiveCompleteStudyIcon.svg'
+import MyPageStateStore from '@/store/mypageStateStore'
+import { useNavigate } from 'react-router'
+import { ROUTE_PATHS } from '@/constant/route'
 function MobileSideNagigation() {
+  const navigate = useNavigate()
+  const myPageState = MyPageStateStore((state) => state.myPageState)
+  const setMyPageState = MyPageStateStore((state) => state.setMyPageState)
   return (
-    // 활성화된 부분 스타일링 추가해야함
     <div className="h-[162px] w-[352px] rounded-xl border-2 border-solid border-gray-200">
       <div className="flex h-1/2 w-full">
-        <div className="flex w-1/2 flex-col items-center justify-center gap-[9px] rounded-xl">
+        <div
+          className={
+            myPageState === 'MY_INFORMATION'
+              ? 'bg-primary-500 flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-tl-xl'
+              : 'flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-xl'
+          }
+          onClick={() => {
+            setMyPageState('MY_INFORMATION')
+            navigate(ROUTE_PATHS.MYPAGE.MY_INFORMATION)
+          }}
+        >
           <img
-            src={profileIcon}
+            src={
+              myPageState === 'MY_INFORMATION' ? activeProfileIcon : profileIcon
+            }
             alt="profileIcon"
             className="h-[24px] w-[24px]"
           />
-          <span className="text-xs text-gray-600">내정보</span>
+          <span
+            className={
+              myPageState === 'MY_INFORMATION'
+                ? 'text-basic-white text-xs'
+                : 'text-xs text-gray-600'
+            }
+          >
+            내정보
+          </span>
         </div>
-        <div className="flex w-1/2 flex-col items-center justify-center gap-[9px] rounded-r-xl">
+        <div
+          className={
+            myPageState === 'BOOKMARK_ANNOUNCEMENT' ||
+            myPageState === 'BOOKMARK_STUDY'
+              ? 'bg-primary-500 flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-tr-xl'
+              : 'flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-r-xl'
+          }
+          onClick={() => {
+            setMyPageState('BOOKMARK_ANNOUNCEMENT')
+            navigate(ROUTE_PATHS.MYPAGE.BOOKMARK_ANNOUNCEMENT)
+          }}
+        >
           <img
-            src={bookMarkIcon}
+            src={
+              myPageState === 'BOOKMARK_ANNOUNCEMENT' ||
+              myPageState === 'BOOKMARK_STUDY'
+                ? activeBookMarkIcon
+                : bookMarkIcon
+            }
             alt="bookMarkIcon"
             className="h-[24px] w-[24px]"
           />
-          <span className="text-xs text-gray-600">북마크</span>
+          <span
+            className={
+              myPageState === 'BOOKMARK_ANNOUNCEMENT' ||
+              myPageState === 'BOOKMARK_STUDY'
+                ? 'text-basic-white text-xs'
+                : 'text-xs text-gray-600'
+            }
+          >
+            북마크
+          </span>
         </div>
       </div>
       <div className="flex h-1/2 w-full">
-        <div className="flex w-1/2 flex-col items-center justify-center gap-[9px] rounded-b-xl">
-          <img src={applyIcon} alt="applyIcon" className="h-[24px] w-[24px]" />
-          <span className="text-xs text-gray-600">지원내역</span>
-        </div>
-        <div className="flex w-1/2 flex-col items-center justify-center gap-[9px] rounded-r-xl">
+        <div
+          className={
+            myPageState === 'APPLY_LIST'
+              ? 'bg-primary-500 flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-bl-xl'
+              : 'flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-b-xl'
+          }
+          onClick={() => {
+            setMyPageState('APPLY_LIST')
+            navigate(ROUTE_PATHS.MYPAGE.APPLY_LIST)
+          }}
+        >
           <img
-            src={completeStudyIcon}
+            src={myPageState === 'APPLY_LIST' ? activeApplyIcon : applyIcon}
+            alt="applyIcon"
+            className="h-[24px] w-[24px]"
+          />
+          <span
+            className={
+              myPageState === 'APPLY_LIST'
+                ? 'text-basic-white text-xs'
+                : 'text-xs text-gray-600'
+            }
+          >
+            지원내역
+          </span>
+        </div>
+        <div
+          className={
+            myPageState === 'COMPLETE_STUDY'
+              ? 'bg-primary-500 flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-br-xl'
+              : 'flex w-1/2 cursor-pointer flex-col items-center justify-center gap-[9px] rounded-r-xl'
+          }
+          onClick={() => {
+            setMyPageState('COMPLETE_STUDY')
+            navigate(ROUTE_PATHS.MYPAGE.COMPLETE_STUDY)
+          }}
+        >
+          <img
+            src={
+              myPageState === 'COMPLETE_STUDY'
+                ? activeCompleteStudyIcon
+                : completeStudyIcon
+            }
             alt="completeStudyIcon"
             className="h-[24px] w-[24px]"
           />
-          <span className="text-xs text-gray-600">완료스터디</span>
+          <span
+            className={
+              myPageState === 'COMPLETE_STUDY'
+                ? 'text-basic-white text-xs'
+                : 'text-xs text-gray-600'
+            }
+          >
+            완료스터디
+          </span>
         </div>
       </div>
     </div>
