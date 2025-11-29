71744d5580579cc077ed5e1b8f9399e462cad54b
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-29 19:07:23 +0900

    feat: EmailVerificationSection 컴포넌트 구현 (이메일 인증)(#38)

--- src/components/signup/EmailVerificationSection.tsx
+++ src/components/signup/EmailVerificationSection.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react'
+import Button from '../common/Button'
+import Input from '../common/Input'
+import FormField from './FormField'
+
 function EmailVerificationSection() {
-  return <div></div>
+  const [email, setEmail] = useState('')
+
+  return (
+    <FormField
+      htmlFor="email"
+      label="이메일"
+      require
+      info="로그인 시 아이디로 사용합니다."
+    >
+      <div className="flex gap-2.5">
+        <Input
+          className="h-12 flex-1"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button
+          disabled={!email}
+          className={`w-[112px] text-base ${email ? 'verify-color hover:verify-color' : 'before-verify-color opacity-60'}`}
+        >
+          중복 확인
+        </Button>
+      </div>
+    </FormField>
+  )
 }
 
 export default EmailVerificationSection
