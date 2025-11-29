94fcc5363f10e3e08a2860f6b124f35395a74a7b
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-29 20:52:34 +0900

    feat: email 인증 Input 추가(#38)

--- src/components/signup/EmailVerificationSection.tsx
+++ src/components/signup/EmailVerificationSection.tsx
@@ -5,28 +5,50 @@ import FormField from './FormField'
 
 function EmailVerificationSection() {
   const [email, setEmail] = useState('')
+  const [verificationCode, setVerificationCode] = useState('')
 
   return (
-    <FormField
-      htmlFor="email"
-      label="이메일"
-      require
-      info="로그인 시 아이디로 사용합니다."
-    >
-      <div className="flex gap-2.5">
-        <Input
-          className="h-12 flex-1"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Button
-          disabled={!email}
-          className={`w-[112px] text-base ${email ? 'verify-color hover:verify-color' : 'before-verify-color opacity-60'}`}
-        >
-          중복 확인
-        </Button>
-      </div>
-    </FormField>
+    <div>
+      <FormField
+        htmlFor="email"
+        label="이메일"
+        require
+        info="로그인 시 아이디로 사용합니다."
+      >
+        <div className="flex gap-2.5">
+          <Input
+            id="email"
+            className="h-12 flex-1"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@gmail.com"
+          />
+          <Button
+            disabled={!email}
+            className={`w-[112px] text-base ${email ? 'verify-color hover:verify-color' : 'before-verify-color opacity-60'}`}
+          >
+            인증번호전송
+          </Button>
+        </div>
+      </FormField>
+      <FormField htmlFor="verificationCode" className="gap-4">
+        <div className="flex gap-2.5">
+          <Input
+            id="verificationCode"
+            className="h-12 flex-1"
+            value={verificationCode}
+            onChange={(e) => setVerificationCode(e.target.value)}
+            placeholder="전송된 코드를 입력해주세요."
+          />
+          <Button
+            disabled={!verificationCode}
+            className={`w-[112px] text-base ${verificationCode ? 'verify-color hover:verify-color' : 'before-verify-color opacity-60'}`}
+          >
+            인증번호확인
+          </Button>
+        </div>
+      </FormField>
+    </div>
   )
 }
 
