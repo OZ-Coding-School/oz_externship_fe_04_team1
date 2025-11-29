52d39fba359ff33a9aeb23448b78e0775a2d215e
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-29 16:22:42 +0900

    style: 회원가입 페이지 레이아웃 구현(#38)

---
+++ src/pages/signupPage.tsx
@@ -0,0 +1,52 @@
+import Button from '@/components/common/Button'
+import BasicInfoSection from '@/components/signup/BasicInfoSection'
+import EmailVerificationSection from '@/components/signup/EmailVerificationSection'
+import PasswordSection from '@/components/signup/PasswordSection'
+import PhoneVerificationSection from '@/components/signup/PhoneVerificationSection'
+import { Link } from 'react-router'
+
+// useForm() 한번 생성
+// FormProvider로 하위 컴포넌트들에 전파
+// 제출처리 ( handleSubmit)
+
+// 하위 컴포넌트로
+// -  BasicInfoSection(이름, 닉네임, 생년월일, 성별)
+// - EmailVerificationSection(이메일 인증)
+// - PhoneVerificationSection(휴대폰 인증)
+// - PasswordSection ( 비밀번호 )
+
+function SignupPage() {
+  return (
+    // 여기서 페이지의 전체적인 레이아웃 적용해야함 (의문: min-h-screen은 100vh를 유지하려고 할텐데 그럼 헤더, 푸터가 적용되면 어떻게 되는거지..?)
+    <div className="mx-auto flex min-h-screen w-full flex-col items-center bg-gray-50">
+      {/* 회원가입 폼 */}
+      <form className="my-14 flex h-[1287px] w-[528px] flex-col gap-9 bg-white px-5 py-10">
+        {/* 헤더 */}
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h2 className="text-3xl font-bold">회원가입</h2>
+          <p className="text-sm text-gray-600">
+            이미 계정이 있으신가요?
+            <Link className="text-primary-600 pl-3" to={'/login'}>
+              로그인하기
+            </Link>
+          </p>
+        </div>
+        <div>
+          {/* 기본적인 정보 입력 (이름, 닉네임, 생년월일, 성별) */}
+          <BasicInfoSection />
+
+          {/* 이메일 입력 */}
+          <EmailVerificationSection />
+
+          {/* 휴대전화 입력 */}
+          <PhoneVerificationSection />
+
+          {/* 비밀번호 입력 */}
+          <PasswordSection />
+        </div>
+        <Button className="w-full">가입하기</Button>
+      </form>
+    </div>
+  )
+}
+export default SignupPage
