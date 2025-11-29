00c70315c97a8c0070fe737095ab0fb5064e1ffa
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-29 21:30:00 +0900

    feat: PasswordSection 컴포넌트 구현(비밀번호)(#38)

--- src/components/signup/PasswordSection.tsx
+++ src/components/signup/PasswordSection.tsx
@@ -1,4 +1,33 @@
+import Input from '../common/Input'
+import FormField from './FormField'
+
 function PasswordSection() {
-  return <div></div>
+  return (
+    <div>
+      <FormField
+        htmlFor="password"
+        require
+        label="비밀번호"
+        info="8~15자의 영문 대소문자, 숫자, 특수문자 포함"
+      >
+        <Input
+          id="name"
+          type="password"
+          className="h-12"
+          placeholder="비밀번호를 입력해주세요"
+          required
+        />
+      </FormField>
+      <FormField htmlFor="passwordCheck" className="gap-4">
+        <Input
+          id="name"
+          type="password"
+          className="h-12"
+          placeholder="비밀번호를 다시 입력해주세요"
+          required
+        />
+      </FormField>
+    </div>
+  )
 }
 export default PasswordSection
