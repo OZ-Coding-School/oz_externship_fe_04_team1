d83fdd5cb7cc056117974e6c59716cf2fbe01929
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-29 18:47:44 +0900

    style: foucs, error일때도 outline -> border로 변경(#38)

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -6,12 +6,12 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 }
 
 const inputVariants = cva(
-  'w-full rounded-lg px-4 py-2.5 text-sm font-normal border-2 placeholder:text-gray-400 transition-all border-gray-300 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:opacity-50',
+  'w-full rounded-lg px-4 py-2.5 text-sm font-normal border-2 placeholder:text-gray-400 transition-all border-gray-300 disabled:cursor-not-allowed focus:outline-none disabled:bg-gray-50 disabled:opacity-50',
   {
     variants: {
       variant: {
-        focus: 'focus:outline-primary-500 focus:outline-2',
-        error: 'outline-danger-100',
+        focus: 'focus:border-primary-500',
+        error: 'border-danger-100',
       },
     },
   }
