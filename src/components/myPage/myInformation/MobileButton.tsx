565e226dd30f889161616e1fff37bbd27daf5f50
cks-wls <slo0521@naver.com>
2025-11-30 15:25:23 +0900

    feat : 비밀번호 변경 모달 구현(#43)

--- src/components/myPage/myInformation/MobileButton.tsx
+++ src/components/myPage/myInformation/MobileButton.tsx
@@ -4,17 +4,31 @@ function MobileButton() {
   const { setIsEditModalOpen } = useOutletContext<{
     setIsEditModalOpen: (value: boolean) => void
   }>()
+  const { setIsEditPassWordModalOpen } = useOutletContext<{
+    setIsEditPassWordModalOpen: (value: boolean) => void
+  }>()
+  // 수정 모달 핸들러
+  const handleEditModalOpen = () => {
+    setIsEditModalOpen(true)
+  }
+  // 비밀번호 변경 모달 핸들러
+  const handleEditPassWordModalOpen = () => {
+    setIsEditPassWordModalOpen(true)
+  }
   return (
-    // 추후 로직 구현
+    // 회원탈퇴 핸들러 로직 추거허가
     <div className="mt-[25px] flex w-full flex-col gap-3">
       <button
         className="bg-primary-500 text-basic-white w-full cursor-pointer rounded-lg py-2"
-        onClick={() => setIsEditModalOpen(true)}
+        onClick={handleEditModalOpen}
       >
         프로필 수정
       </button>
       <div className="flex gap-2">
-        <button className="text-basic-white w-1/2 cursor-pointer rounded-lg bg-gray-500 py-2">
+        <button
+          className="text-basic-white w-1/2 cursor-pointer rounded-lg bg-gray-500 py-2"
+          onClick={handleEditPassWordModalOpen}
+        >
           비밀번호 변경
         </button>
         <button className="text-basic-white bg-danger-500 w-1/2 cursor-pointer rounded-lg py-2">
---
+++ src/components/myPage/myInformation/modal/EditPassWordModal.tsx
@@ -0,0 +1,63 @@
+import closeIcon from '@/assets/icons/close.svg'
+import Button from '@/components/common/Button'
+import Input from '@/components/common/Input'
+interface EditPassWordModalProps {
+  setIsEditPassWordModalOpen: (value: boolean) => void
+}
+function EditPassWordModal({
+  setIsEditPassWordModalOpen,
+}: EditPassWordModalProps) {
+  const handleModalClose = () => {
+    setIsEditPassWordModalOpen(false)
+  }
+  return (
+    <div className="bg-basic-white h-[457px] w-[448px] flex-col gap-6 rounded-xl border py-8">
+      <div className="flex items-center justify-between border-b-2 border-gray-200 px-6 pb-8">
+        <span>비밀번호 변경</span>
+        <img
+          src={closeIcon}
+          alt="closeIcon"
+          className="cursor-pointer"
+          onClick={handleModalClose}
+        />
+      </div>
+      {/* 폼 */}
+      {/* 추후 유효성 검사 로직 추가하기 */}
+      <div className="flex flex-col gap-4 px-6 py-6">
+        <div className="flex flex-col gap-2">
+          <div className="flex">
+            <span className="text-gray-700b text-sm">현재 비밀번호</span>
+            <span className="text-danger-500 text-sm">*</span>
+          </div>
+          <Input placeholder="현재 비밀번호를 입력해주세요" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex">
+            <span className="text-gray-700b text-sm">새 비밀번호</span>
+            <span className="text-danger-500 text-sm">*</span>
+          </div>
+          <Input placeholder="새 비밀번호를 입력해주세요(8자 이상)" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex">
+            <span className="text-gray-700b text-sm">새 비밀번호 확인</span>
+            <span className="text-danger-500 text-sm">*</span>
+          </div>
+          <Input placeholder="새 비밀번호를 다시 입력해주세요" />
+        </div>
+      </div>
+      {/* 버튼 */}
+      <div className="mt-6 flex justify-end gap-3 px-6">
+        <Button
+          variant="outline"
+          onClick={handleModalClose}
+          className="cursor-pointer"
+        >
+          취소
+        </Button>
+        <Button className="cursor-pointer">변경하기</Button>
+      </div>
+    </div>
+  )
+}
+export default EditPassWordModal
