93dcf3339b9406e6715a08b02c83a060b9570a2a
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-01 18:07:52 +0900

    feat: 회원가입 폼 타입 구현(#46)

---
+++ src/types/signup.ts
@@ -0,0 +1,10 @@
+export type SignupFormValues = {
+  name: string
+  nickname: string
+  birthday: string
+  gender: 'M' | 'F' | ''
+  email: string
+  phone_number: string
+  password: string
+  password_confirm: string
+}
