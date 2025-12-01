6f746e7c6f4137e2d4286ccc64db277575a27fde
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-01 18:10:45 +0900

    feat: PasswordSection 유효성 검사 로직 구현(#46)

--- src/components/signup/PasswordSection.tsx
+++ src/components/signup/PasswordSection.tsx
@@ -1,7 +1,30 @@
+import { useFormContext } from 'react-hook-form'
 import Input from '../common/Input'
 import FormField from './FormField'
+import type { SignupFormValues } from '@/types/signup'
 
 function PasswordSection() {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext<SignupFormValues>()
+  const password = watch('password')
+
+  const passwrodRegister = register('password', {
+    required: '비밀번호를 입력해주세요.',
+    pattern: {
+      value:
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?-]).{8,15}$/,
+      message: '8~15자, 대소문자+숫자+특수문자 포함 해주세요',
+    },
+  })
+
+  const passwordConfirmRegister = register('password_confirm', {
+    required: '비밀번호를 다시 입력해주세요.',
+    validate: (value) => value === password || '비밀번호가 일치하지 않습니다.',
+  })
+
   return (
     <div>
       <FormField
@@ -9,24 +32,31 @@ function PasswordSection() {
         require
         label="비밀번호"
         info="8~15자의 영문 대소문자, 숫자, 특수문자 포함"
+        errorMsg={errors.password?.message}
       >
         <Input
+          {...passwrodRegister}
           id="password"
           type="password"
           autoComplete="new-password"
           className="h-12"
+          error={!!errors.password}
           placeholder="비밀번호를 입력해주세요"
-          required
         />
       </FormField>
-      <FormField htmlFor="passwordConfirm" className="gap-4">
+      <FormField
+        htmlFor="password_confirm"
+        className="gap-4"
+        errorMsg={errors.password_confirm?.message}
+      >
         <Input
-          id="passwordConfirm"
+          {...passwordConfirmRegister}
+          id="password_confirm"
           type="password"
           autoComplete="new-password"
           className="h-12"
+          error={!!errors.password_confirm}
           placeholder="비밀번호를 다시 입력해주세요"
-          required
         />
       </FormField>
     </div>
