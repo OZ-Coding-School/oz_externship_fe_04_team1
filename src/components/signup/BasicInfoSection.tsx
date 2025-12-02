fc042c39c9dd7f3940a4e720c01f650ca2739d51
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-02 18:33:32 +0900

    feat: 닉네임 중복 확인 API를 useCheckNickname 훅과 연동(#59)

--- src/components/signup/BasicInfoSection.tsx
+++ src/components/signup/BasicInfoSection.tsx
@@ -3,6 +3,8 @@ import Button from '../common/Button'
 import Input from '../common/Input'
 import FormField from './FormField'
 import { useFormContext, useWatch } from 'react-hook-form'
+import { useCheckNickname } from '@/hooks/quries/auth/useSignup'
+import { useState } from 'react'
 
 function BasicInfoSection() {
   const {
@@ -11,6 +13,11 @@ function BasicInfoSection() {
   } = useFormContext<SignupFormValues>()
   const nickname = useWatch({ name: 'nickname' })
   const gender = useWatch({ name: 'gender' })
+  const { mutate: checkNickname, isPending: isCheckingNickname } =
+    useCheckNickname()
+  const [isNicknameAvailable, setIsNicknameAvailable] = useState<
+    boolean | null
+  >(null)
 
   const nameRegister = register('name', {
     required: '이름을 입력해주세요',
@@ -26,6 +33,7 @@ function BasicInfoSection() {
       value: /^[가-힣a-zA-Z0-9]{1,10}$/,
       message: '1~10자의 한글/영문/숫자만 가능합니다.',
     },
+    onChange: () => setIsNicknameAvailable(null),
   })
 
   const birthdayRegister = register('birthday', {
@@ -39,6 +47,21 @@ function BasicInfoSection() {
   const genderRegister = register('gender', {
     required: '성별을 선택해주세요',
   })
+
+  const handleCheckNickname = (nickname: string) => {
+    if (errors.nickname) return
+    checkNickname(
+      { nickname },
+      {
+        onSuccess: () => {
+          // 닉네임 사용 가능
+          setIsNicknameAvailable(true)
+        },
+        // 이미 사용중인 닉네임
+        onError: () => setIsNicknameAvailable(false),
+      }
+    )
+  }
 
   return (
     <div className="flex flex-col gap-11">
@@ -76,12 +99,22 @@ function BasicInfoSection() {
             placeholder="닉네임을 입력해주세요"
           />
           <Button
-            disabled={!nickname}
+            disabled={!nickname || !!errors.nickname}
             className={`w-[112px] text-base ${nickname ? 'verify-color hover:verify-color' : 'before-verify-color opacity-60'}`}
+            onClick={() => handleCheckNickname(nickname)}
           >
             중복 확인
           </Button>
         </div>
+        {!errors.nickname && isNicknameAvailable !== null && (
+          <p
+            className={`pt-2 pl-1 text-sm ${isNicknameAvailable ? 'text-success-600' : 'text-danger-500'}`}
+          >
+            {isNicknameAvailable
+              ? '사용가능한 닉네임입니다.'
+              : '이미 존재하는 닉네임입니다.'}
+          </p>
+        )}
       </FormField>
 
       {/* 생년월일 */}
