5284d3d43c4dffd878eaff40c5eb7bff1c9631b2
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-02 15:30:15 +0900

    feat: 회원가입 관련 API 모킹 코드 구현(msw)(#59)

--- src/types/signup.ts
+++ src/types/signup.ts
@@ -1,3 +1,21 @@
+export type ReqInfo = Omit<SignupFormValues, 'password_confirm'>
+
+export type ReqEmailOnly = Pick<SignupFormValues, 'email'>
+
+export type ReqPhoneOnly = Pick<SignupFormValues, 'phone_number'>
+
+export type ReqNicknameOnly = Pick<SignupFormValues, 'nickname'>
+
+export type ReqCodeWithEmail = {
+  email: string
+  code: string
+}
+
+export type ReqCodeWithPhone = {
+  phone_number: string
+  code: string
+}
+
 export type SignupFormValues = {
   name: string
   nickname: string
