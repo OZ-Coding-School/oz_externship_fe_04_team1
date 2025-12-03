c556c3429349de1980431d31a06c122cbb60688d
cks-wls <slo0521@naver.com>
2025-12-03 13:42:38 +0900

    style : md -> sm으로 수정(#65)

--- src/components/common/cards/StudyCompleteCard.tsx
+++ src/components/common/cards/StudyCompleteCard.tsx
@@ -18,17 +18,17 @@ function StudyCompleteCard({
     (_, i) => i < Math.floor(average_rating)
   )
   return (
-    <div className="max-h-[500px] w-[318px] rounded-lg border border-gray-200 md:w-[384px]">
+    <div className="max-h-[500px] w-[318px] rounded-lg border border-gray-200 sm:w-[384px]">
       {/* thumbnail */}
       <img
-        className="h-[179px] w-full rounded-t-lg object-cover md:h-[216px]"
+        className="h-[179px] w-full rounded-t-lg object-cover sm:h-[216px]"
         src={thumbnail_img_url}
         alt={name}
       />
 
       {/* content */}
-      <div className="p-3 md:p-5">
-        <div className="flex items-center justify-between pb-2 md:pb-3">
+      <div className="p-3 sm:p-5">
+        <div className="flex items-center justify-between pb-2 sm:pb-3">
           <h4 className="text-md font-medium md:text-lg">{name}</h4>
           {is_leader && (
             <span className="bg-primary-100 text-primary-800 rounded-full px-2 py-1 text-xs">
@@ -37,7 +37,7 @@ function StudyCompleteCard({
           )}
         </div>
 
-        <ul className="flex flex-col justify-center gap-1 pb-4 text-xs text-gray-600 md:gap-2 md:text-sm">
+        <ul className="flex flex-col justify-center gap-1 pb-4 text-xs text-gray-600 sm:gap-2 sm:text-sm">
           <li className="flex items-center gap-1">
             <Clock3 className="h-4 w-3" /> 기간: {duration}
           </li>
@@ -53,7 +53,7 @@ function StudyCompleteCard({
         {/* review */}
         {review ? (
           <>
-            <div className="mb-3 flex flex-col gap-2 rounded-lg bg-gray-50 p-4 md:mb-4">
+            <div className="mb-3 flex flex-col gap-2 rounded-lg bg-gray-50 p-4 sm:mb-4">
               <div className="flex items-center justify-between text-gray-600">
                 <div className="flex items-center">
                   {stars.map((filled, idx) => (
@@ -68,16 +68,16 @@ function StudyCompleteCard({
                 </div>
                 <SquarePen
                   onClick={onReviewClick}
-                  className="hidden h-[17px] w-4 cursor-pointer md:block"
+                  className="hidden h-[17px] w-4 cursor-pointer sm:block"
                 />
               </div>
-              <p className="line-clamp-2 text-xs text-gray-600 md:text-sm">
+              <p className="line-clamp-2 text-xs text-gray-600 sm:text-sm">
                 {review}
               </p>
             </div>
             <Button
               variant="primary"
-              className="block w-full p-2 md:hidden md:h-[40px]"
+              className="block w-full p-2 sm:hidden sm:h-[40px]"
               onClick={onReviewClick}
             >
               리뷰 수정
@@ -85,13 +85,13 @@ function StudyCompleteCard({
           </>
         ) : (
           <>
-            <div className="bg-primary-50 text-primary-800 mb-3 flex items-center justify-center rounded-lg p-2 text-xs font-normal md:mb-4 md:h-[53px] md:text-sm">
+            <div className="bg-primary-50 text-primary-800 mb-3 flex items-center justify-center rounded-lg p-2 text-xs font-normal sm:mb-4 sm:h-[53px] sm:text-sm">
               아직 리뷰를 작성하지 않았습니다
             </div>
             <Button
               onClick={onReviewClick}
               variant="primary"
-              className="w-full p-2 md:h-[40px]"
+              className="w-full p-2 sm:h-[40px]"
             >
               리뷰 작성
             </Button>
