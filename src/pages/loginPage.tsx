ad16a664184b88798547b72c930e090231a20846
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-03 17:31:48 +0900

    refactor: 라우팅 경로를 ROUTE_PATHS로 통합

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,6 +1,7 @@
 import logoImg from '@/assets/images/logo.svg'
 import LoginForm from '@/components/login/LoginForm'
 import SocialLogin from '@/components/login/SocialLogin'
+import { ROUTE_PATHS } from '@/constant/route'
 import { Link } from 'react-router'
 
 function LoginPage() {
@@ -16,7 +17,7 @@ function LoginPage() {
         <h3 className="mt-1 text-3xl font-bold">로그인</h3>
         <p className="mt-2 text-sm font-light text-gray-600">
           아직 계정이 없으신가요?{' '}
-          <Link className="text-primary-600" to={'/signup'}>
+          <Link className="text-primary-600" to={ROUTE_PATHS.SIGNUP}>
             회원가입하기
           </Link>
         </p>
