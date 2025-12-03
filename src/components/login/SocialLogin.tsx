50e890d6db9eed56e7c9a45e5a7e61afc5c89152
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-03 15:26:37 +0900

    feat: 소셜 로그인 컴포넌트 구현(#69)

---
+++ src/components/login/SocialLogin.tsx
@@ -0,0 +1,24 @@
+import kakao from '@/assets/icons/kakao.svg'
+import naver from '@/assets/icons/naver.svg'
+
+function SocialLogin() {
+  return (
+    <div className="flex flex-col gap-3">
+      <button
+        type="button"
+        className="inline-flex h-[52px] cursor-pointer items-center justify-center rounded-md bg-[rgb(255,230,3)] text-center text-base font-normal text-[rgb(56,28,26)]"
+      >
+        <img className="pr-2" src={kakao} alt="" />
+        카카오 간편 로그인 / 가입
+      </button>
+      <button
+        type="button"
+        className="inline-flex h-[52px] cursor-pointer items-center justify-center rounded-md bg-[rgb(4,199,92)] text-base font-normal text-white"
+      >
+        <img className="pr-2" src={naver} alt="" /> 네이버 간편 로그인 / 가입
+      </button>
+    </div>
+  )
+}
+
+export default SocialLogin
