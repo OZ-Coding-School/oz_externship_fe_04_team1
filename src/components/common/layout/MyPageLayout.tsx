f45bfa87c9e3c82d4581efef10da89657c65a407
cks-wls <slo0521@naver.com>
2025-12-04 17:39:16 +0900

    refactor : 지원 내역 모달 상태관리 리펙토링(#81)

--- src/components/common/layout/MyPageLayout.tsx
+++ src/components/common/layout/MyPageLayout.tsx
@@ -2,17 +2,12 @@ import Header from '@/components/common/header/Header'
 import { useState } from 'react'
 import { Outlet } from 'react-router'
 import SideNavigation from '@/components/common/sideNavigation/SideNavigation'
-import ApplyListModal from '@/components/myPage/applyList/ApplyListModal'
 import CompleteStudyReviewModal from '@/components/myPage/completeStudy/CompleteStudyReviewModal'
 import type { Review } from '@/types/review'
 import { MyInformationModalProvider } from '@/store/context/myInformationModalContext'
 function MyPageLayout() {
   // 사이드바 상태
   const [isSideBarOpen, setIsSideBarOpen] = useState(false)
-  // 지원 내역 상세 모달 상태
-  const [isApplyListModalOpen, setIsApplyListModalOpen] = useState(false)
-  // 선택된 항목에 대한 id 상태 관리
-  const [applyListId, setApplyListId] = useState<number | null>(null)
   // 리뷰 모달 상태
   const [isReviewModalOpen, setIsReviewModalOpen] = useState(false)
   // 어떤 완료된 스터디에 관한 정보인지 상태
@@ -34,8 +29,6 @@ function MyPageLayout() {
             <div className="bg-basic-white w-full rounded-xl border border-solid border-gray-200 px-8 py-8">
               <Outlet
                 context={{
-                  setIsApplyListModalOpen: setIsApplyListModalOpen,
-                  setApplyListId: setApplyListId,
                   setIsReviewModalOpen: setIsReviewModalOpen,
                   setCompleteStudyStore: setCompleteStudyStore,
                 }}
@@ -45,14 +38,6 @@ function MyPageLayout() {
         </div>
         {isSideBarOpen && (
           <div className="fixed inset-0 h-full w-full bg-black opacity-50 md:hidden"></div>
-        )}
-        {isApplyListModalOpen && (
-          <div className="fixed inset-0 flex h-full w-full items-center justify-center bg-black/50 px-5">
-            <ApplyListModal
-              setIsApplyListModalOpen={setIsApplyListModalOpen}
-              applyListId={applyListId}
-            />
-          </div>
         )}
         {isReviewModalOpen && (
           <div className="fixed inset-0 flex h-full w-full items-center justify-center bg-black/50 px-5">
