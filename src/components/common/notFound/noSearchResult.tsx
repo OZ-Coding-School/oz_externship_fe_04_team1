15494171573a6100b500a2906c00238e4ce30c06
cks-wls <slo0521@naver.com>
2025-12-08 13:03:10 +0900

    feat : 검색 결과 없음 컴포넌트 수정(#98)

--- src/components/common/notFound/noSearchResult.tsx
+++ src/components/common/notFound/noSearchResult.tsx
@@ -1,7 +1,9 @@
 import Search from '@/assets/search.svg'
 import Button from '@/components/common/Button'
-
-const NoSearchReult = () => {
+interface NoSearchResultProps {
+  searchResult: string
+}
+const NoSearchReult = ({ searchResult }: NoSearchResultProps) => {
   return (
     <div className="h-[382px] w-[854px] justify-center rounded-[16px] border border-gray-200 bg-gray-50 p-[25px] text-center">
       <div className="flex h-full flex-col items-center justify-center text-center">
@@ -16,17 +18,8 @@ const NoSearchReult = () => {
         </div>
 
         <p className="mb-[8px] text-[20px] font-bold text-gray-700">
-          검색 결과가 없습니다
+          {`'${searchResult}'`} 에 대한 검색 결과가 없습니다
         </p>
-        <p className="mb-[24px] text-[16px] text-gray-700">
-          다른 키워드로 검색해보시거나 필터를 조정해주세요
-        </p>
-        <div className="flex gap-3">
-          <Button size="lg">필터초기화</Button>
-          <Button size="lg" variant="outline">
-            새로운 검색
-          </Button>
-        </div>
       </div>
     </div>
   )
