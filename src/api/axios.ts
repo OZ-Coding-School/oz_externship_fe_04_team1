90c1ff4401b32689dc762fe56e92d05128010fd3
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-08 11:51:36 +0900

    feat: interceptor request에서 access token을 헤더에 자동 추가하도록 구현(#99)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,5 @@
 import { API_BASE_URL } from '@/constant/api'
+import AuthStateStore from '@/store/authStateStore'
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
@@ -6,6 +7,16 @@ export const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
+})
+
+// 토큰이 있으면 헤더에 추가
+axiosInstance.interceptors.request.use((config) => {
+  const token = AuthStateStore.getState().accessToken
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 
 // response.data에 statusCode를 추가해서 throw
