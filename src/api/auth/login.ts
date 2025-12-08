5b2ff1251403ae6bb33ac727526fd07c446c0f48
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-08 11:52:26 +0900

    feat: 응답에서 받은 access token을 AuthStateStore에 저장(#99)

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -1,11 +1,15 @@
 import type { ReqLoginFormData, ResLoginData } from '@/types/login'
 import { axiosInstance } from '../axios'
 import { API_PATHS } from '@/constant/api'
+import AuthStateStore from '@/store/authStateStore'
 
 export const loginWithEmail = async (
   data: ReqLoginFormData
 ): Promise<ResLoginData> => {
   const res = await axiosInstance.post(API_PATHS.LOGIN.EMAIL.POST, data)
+
+  const { access_token } = res.data
+  AuthStateStore.getState().setAccessToken(access_token)
 
   return res.data
 }
