e41fcfac55b7ab514475ebc719c87f6ca388ab02
cks-wls <slo0521@naver.com>
2025-12-08 10:37:55 +0900

    feat : 페이지 상단으로 이동하는 컴포넌트 구현(#96)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import AppRoutes from '@/routes/AppRoutes'
 import { useLocation } from 'react-router'
 import { ToastContainer } from 'react-toastify'
+import MoveToTop from '@/components/common/moveToTop/MoveToTop'
 
 function App() {
   const { pathname } = useLocation()
-
   const isSpecialPage = pathname.startsWith('/mypage')
   return (
     <>
       <ToastContainer position={isSpecialPage ? 'top-left' : 'top-center'} />
+      <MoveToTop />
       <AppRoutes />
     </>
   )
