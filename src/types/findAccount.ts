2d5dac1136e6133470de83fa8fdffad7fe8e4d27
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-12 01:51:51 +0900

    feat: 비밀번호, 이메일 찾기 타입 구현(#115)

---
+++ src/types/findAccount.ts
@@ -0,0 +1,16 @@
+export enum FINDTYPE {
+  FIND_EMAIL = 1,
+  FIND_PASSWORD = 2,
+}
+
+export type StepIndicatorProps = {
+  title: string
+  subTitle: string
+  type: FINDTYPE
+}
+
+export enum StepIndicatorType {
+  AUTH = 1,
+  VERIFY = 2,
+  COMPLETE = 3,
+}
