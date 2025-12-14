372a4f6f80689974be588be688e9296c2e6e364f
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-14 17:25:39 +0900

    refactor: 비밀번호 찾기 페이지 구현(#122)

--- src/pages/findPasswordPage.tsx
+++ src/pages/findPasswordPage.tsx
@@ -1,13 +1,48 @@
-import StepIndicator from '@/components/find/StepIndicator'
-import { FINDTYPE } from '@/types/findAccount'
+import FindAccountLayout from '@/components/find/common/FindAccountLayout'
+import PasswordAuthStep from '@/components/find/findPassword/PasswordAuthStep'
+import PasswordCompleteStep from '@/components/find/findPassword/PasswordComplete'
+import PasswordVerifyStep from '@/components/find/findPassword/PasswordVerifyStep'
+import {
+  StepIndicatorType,
+  type FindPasswordFormData,
+  type ReqResetPassword,
+} from '@/types/findAccount'
+import { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 function FindPasswordPage() {
+  const [currentStep, setCurrentStep] = useState(StepIndicatorType.AUTH)
+  const methods = useForm<FindPasswordFormData>({ mode: 'onChange' })
+
+  const handleResetPassword = (password: ReqResetPassword) => {
+    // api 작업
+  }
   return (
-    <StepIndicator
-      title={'비밀번호 찾기'}
-      subTitle={'가입한 이메일로 비밀번호를 재설정할 수 있습니다.'}
-      type={FINDTYPE.FIND_PASSWORD}
-    />
+    <FormProvider {...methods}>
+      <FindAccountLayout
+        title="비밀번호 찾기"
+        subTitle="가입하신 이메일을 입력하면 인증코드를 보내드립니다."
+      >
+        {currentStep === StepIndicatorType.AUTH && (
+          <PasswordAuthStep
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep === StepIndicatorType.VERIFY && (
+          <PasswordVerifyStep
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep === StepIndicatorType.COMPLETE && (
+          <PasswordCompleteStep
+            onResetPassword={handleResetPassword}
+            currentStep={currentStep}
+          />
+        )}
+      </FindAccountLayout>
+    </FormProvider>
   )
 }
 
