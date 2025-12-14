b2195c4861f4a8934ba3dffbb5b757f03129fa5e
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-14 16:11:13 +0900

    refactor: 이메일 찾기 단계 컴포넌트 분리 구조로 리팩토링(#122)

--- src/pages/findEmailPage.tsx
+++ src/pages/findEmailPage.tsx
@@ -1,13 +1,37 @@
-import StepIndicator from '@/components/find/StepIndicator'
-import { FINDTYPE } from '@/types/findAccount'
+import FindAccountLayout from '@/components/find/common/FindAccountLayout'
+import EmailCompleteStep from '@/components/find/findEmail/EmailCompleteStep'
+import EmailAuthStep from '@/components/find/findEmail/EmailAuthStep'
+import EmailVerifyStep from '@/components/find/findEmail/EmailVerifyStep'
+import { StepIndicatorType, type FindEmailFormData } from '@/types/findAccount'
+import { useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 
 function FindEmailPage() {
+  const [currentStep, setCurrentStep] = useState(StepIndicatorType.AUTH)
+  const methods = useForm<FindEmailFormData>({ mode: 'onChange' })
   return (
-    <StepIndicator
-      title={'이메일 찾기'}
-      subTitle={'가입 시 입력한 정보로 이메일을 찾을 수 있습니다.'}
-      type={FINDTYPE.FIND_EMAIL}
-    />
+    <FormProvider {...methods}>
+      <FindAccountLayout
+        title="이메일 찾기"
+        subTitle="가입시 입력한 정보로 이메일을 찾을 수 있습니다."
+      >
+        {currentStep === StepIndicatorType.AUTH && (
+          <EmailAuthStep
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep === StepIndicatorType.VERIFY && (
+          <EmailVerifyStep
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep === StepIndicatorType.COMPLETE && (
+          <EmailCompleteStep currentStep={currentStep} />
+        )}
+      </FindAccountLayout>
+    </FormProvider>
   )
 }
 
