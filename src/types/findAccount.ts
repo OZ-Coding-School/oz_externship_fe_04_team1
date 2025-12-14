0dda7860017b2bb6a583bf255dc5efe0078dfc7e
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-14 18:23:23 +0900

    feat: api 관련 타입 추가(#122)

--- src/types/findAccount.ts
+++ src/types/findAccount.ts
@@ -17,21 +17,6 @@ export enum StepIndicatorType {
   COMPLETE = 3,
 }
 
-export type FindEmailFormData = {
-  name: string
-  phone: string
-}
-
-export type FindPasswordFormData = {
-  email: string
-  password: string
-  password_confirm: string
-}
-
-export type ReqResetPassword = {
-  password: string
-}
-
 export type StepControlProps = {
   currentStep: StepIndicatorType
   setCurrentStep: Dispatch<SetStateAction<StepIndicatorType>>
@@ -39,4 +24,46 @@ export type StepControlProps = {
 
 export type CompleteStepProps = {
   currentStep: StepIndicatorType
+}
+
+// 비밀번호 찾기
+export type FindPasswordFormData = {
+  email: string
+  code: string
+  password: string
+  password_confirm: string
+}
+
+export type ReqVerifyEmailCode = {
+  email: string
+  code: string
+}
+
+export interface PasswordVerifyStepProps extends StepControlProps {
+  onVerifyCode: (data: ReqVerifyEmailCode) => void
+}
+
+export type ReqResetPassword = {
+  email: string
+  new_password: string
+}
+
+export interface PasswordCompleteStepProps extends CompleteStepProps {
+  onResetPassword: (data: ReqResetPassword) => void
+}
+
+// 이메일 찾기
+export type FindEmailFormData = {
+  name: string
+  phone: string
+  code: string
+}
+
+export type ReqVerifyPhoneCode = {
+  phone: string
+  code: string
+}
+
+export interface EmailVerifyStepProps extends StepControlProps {
+  onVerifyCode: (data: ReqVerifyPhoneCode) => void
 }
