11f95487caaaaabfb4fb42edfb993bacc359eed2
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-15 18:34:59 +0900

    feat: 비밀번호 찾기 페이지에 React Query 성공/실패 핸들링 구현

--- src/hooks/quries/findAccount/findPassword.ts
+++ src/hooks/quries/findAccount/findPassword.ts
@@ -12,18 +12,21 @@ import type {
 } from '@/types/findAccount'
 import { useMutation } from '@tanstack/react-query'
 
+// 이메일  전송
 export const usePasswordResetWithEmail = () => {
   return useMutation<ResFindAccount, findAccount400Error, ReqVerifyWithEmail>({
     mutationFn: passwordResetWithEmail,
   })
 }
 
+// 이메일, 인증코드 전송
 export const useVerifyPasswordResetCode = () => {
   return useMutation<ResFindAccount, findAccount400Error, ReqVerifyEmailCode>({
     mutationFn: verifyPasswordResetCode,
   })
 }
 
+// 이메일, 새 비밀번호 전송
 export const useResetPassword = () => {
   return useMutation<ResFindAccount, findAccount400Error, ReqResetPassword>({
     mutationFn: resetPassword,
