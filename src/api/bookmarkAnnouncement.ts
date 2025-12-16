465fff3de9c2c1f583423ddddab06b6be31625f5
cks-wls <slo0521@naver.com>
2025-12-16 13:40:21 +0900

    fix : page_size 지정(#133)

--- src/api/bookmarkAnnouncement.ts
+++ src/api/bookmarkAnnouncement.ts
@@ -3,14 +3,15 @@ import type { BookmarkAnnouncement } from '@/types/bookmarkAnnouncement'
 import { axiosInstance } from '@/api/axios'
 
 export const getBookmarkAnnouncementApi = async (
-  cursor: string | null = null
+  cursor: string | null = null,
+  page_size: number
 ): Promise<BookmarkAnnouncement> => {
   const { data } = await axiosInstance.get(
     API_PATHS.BOOKMARK.ANNOUNCEMENT.GET,
     {
       params: {
         cursor,
-        page_size: 6,
+        page_size,
       },
     }
   )
