cec9326c303eef25806b8631102d4ad4656f3105
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-18 15:19:43 +0900

    refactor: 토스트 알림 우측 상단으로 위치 고정(#159)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import AppRoutes from '@/routes/AppRoutes'
-import { useLocation } from 'react-router'
+
 import { ToastContainer } from 'react-toastify'
 import MoveToTop from '@/components/common/moveToTop/MoveToTop'
-import { ROUTE_PATHS } from './constant/route'
 
 function App() {
-  const { pathname } = useLocation()
-  const isMyPage = pathname.startsWith(ROUTE_PATHS.MYPAGE)
-  const isLoginPage = pathname.startsWith(ROUTE_PATHS.LOGIN)
-  const isSignupPage = pathname.startsWith(ROUTE_PATHS.SIGNUP)
-  const isSpecialPage = isMyPage || isLoginPage || isSignupPage
   return (
     <>
-      <ToastContainer position={isSpecialPage ? 'top-right' : 'top-center'} />
+      <ToastContainer />
       <MoveToTop />
       <AppRoutes />
     </>
