2625650eca727b6ff84cc038a7d252595c5f7528
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-12-18 15:05:27 +0900

    refactor: 회원가입, 로그인 토스트 알림 우측 상단으로 위치 변경(#159)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import AppRoutes from '@/routes/AppRoutes'
 import { useLocation } from 'react-router'
 import { ToastContainer } from 'react-toastify'
 import MoveToTop from '@/components/common/moveToTop/MoveToTop'
+import { ROUTE_PATHS } from './constant/route'
 
 function App() {
   const { pathname } = useLocation()
-  const isSpecialPage = pathname.startsWith('/mypage')
+  const isMyPage = pathname.startsWith(ROUTE_PATHS.MYPAGE)
+  const isLoginPage = pathname.startsWith(ROUTE_PATHS.LOGIN)
+  const isSignupPage = pathname.startsWith(ROUTE_PATHS.SIGNUP)
+  const isSpecialPage = isMyPage || isLoginPage || isSignupPage
   return (
     <>
       <ToastContainer position={isSpecialPage ? 'top-right' : 'top-center'} />
