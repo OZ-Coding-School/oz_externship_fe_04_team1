d653960144f548f325a43ce5d54d8a07c296b90a
cks-wls <slo0521@naver.com>
2025-12-22 18:35:59 +0900

    feat : 북마크한 공고 라우팅 추가(#174)

--- src/components/myPage/bookmarkAnnouncement/MobileBookMark.tsx
+++ src/components/myPage/bookmarkAnnouncement/MobileBookMark.tsx
@@ -102,7 +102,12 @@ function MobileBookMark() {
                   key={value.id}
                   announcementBookmarkData={value.recruitment}
                   onBookmarkClick={() => deleteBookMarkAnnouncement(value.id)}
-                  onViewClick={() => console.log('view clicked')}
+                  onViewClick={() =>
+                    window.open(
+                      `https://learn.ozcoding.site/recruitments/${value.recruitment.uuid}`,
+                      '_blank'
+                    )
+                  }
                   className={
                     optionIsSelected === 'ANNOUNCEMENT' ? 'block' : 'hidden'
                   }
@@ -129,7 +134,12 @@ function MobileBookMark() {
                   key={value.id}
                   announcementBookmarkData={value.recruitment}
                   onBookmarkClick={() => deleteBookMarkAnnouncement(value.id)}
-                  onViewClick={() => console.log('view clicked')}
+                  onViewClick={() =>
+                    window.open(
+                      `https://learn.ozcoding.site/recruitments/${value.recruitment.uuid}`,
+                      '_blank'
+                    )
+                  }
                   className={
                     optionIsSelected === 'ANNOUNCEMENT' ? 'block' : 'hidden'
                   }
